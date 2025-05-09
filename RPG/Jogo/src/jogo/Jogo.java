f0c9614ef6e3c369f4a072188fb4ebb00aeffa46
Mateus Stapani <mateusstapani@gmail.com>
2025-05-09 19:56:14 -0300

    Repositorio criado com Jogo.java adicionado

---
+++ RPG/Jogo/src/jogo/Jogo.java
@@ -0,0 +1,13 @@
+package jogo;
+import java.util.Scanner;
+/**
+ *
+ * Arthur Vieira - Guilherme Sanches - Joao Paulo Serafim - Mateus Stapani - Patrick Assis
+ */
+public class Jogo {
+
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    }
+}
