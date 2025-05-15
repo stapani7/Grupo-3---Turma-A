1cf77897ed4a1348d0951d7fd09085b7e6d539f6
Mateus Stapani <mateusstapani@gmail.com>
2025-05-14 23:20:19 -0300

    desafio2 e desafio3 add

--- RPG/Jogo/src/jogo/jogo/Jogo.java
+++ RPG/Jogo/src/jogo/jogo/Jogo.java
@@ -46,8 +46,36 @@ public class Jogo {
         System.out.println("Agora que você já sabe como funciona o jogo, vamos para o próximo desafio");
     }
 
+    public static void desafio2() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nDesafio 2: Qual será a saída desse algoritmo?");
+        System.out.println("int x = 5;\nint y = 3;\nif(x > y) {\n    System.out.println(x + y);\n} else {\n    System.out.println(x - y);\n}");
+        System.out.print("Digite a resposta correta: ");
+        int resposta = input.nextInt();
+        if (resposta == 8) {
+            System.out.println("Correto! Vamos para o próximo desafio.");
+        } else {
+            System.out.println("Errado! A resposta correta era 8. Vamos continuar.");
+        }
+    }
+
+    public static void desafio3() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nDesafio 3: Quantas vezes o texto será impresso?");
+        System.out.println("for(int i = 0; i < 4; i++) {\n    System.out.println(\"Algoritmo\");\n}");
+        System.out.print("Digite o número de vezes: ");
+        int resposta = input.nextInt();
+        if (resposta == 4) {
+            System.out.println("Muito bem! Próximo desafio.");
+        } else {
+            System.out.println("Resposta incorreta! O correto era 4.");
+        }
+    }
+
     public static void main(String[] args) {
         coletarDadosJogador();
         desafio1();
+        desafio2();
+        desafio3();
     }
 }
