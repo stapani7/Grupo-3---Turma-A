e0a96c3a052fcf8b72ee5631bb77d1c65c814a44
arthur <arthur1vieira2@GMAIL.COM>
2025-05-14 21:45:34 -0300

    desafio1

--- RPG/Jogo/src/jogo/jogo/Jogo.java
+++ RPG/Jogo/src/jogo/jogo/Jogo.java
@@ -6,6 +6,25 @@ import java.util.Scanner;
  */
 public class Jogo {
 
+    public static int desafio1(){
+        System.out.println("Bem vindo ao seu primeiro desafio se você está aqui\n" +
+                " tem o potencial de mudar o mundo problemático em que vivemows\n" +
+                " seu primeiro desafio é numerar a ordem desses acontecimentos \n" +
+                " vamos ver se você sabe algo de algoritmos.");
+        System.out.println("1. Sair do banco\n" +
+                "2.Aguardar a vez de ser atendido\n" +
+                "3.Digitar a senha\n" +
+                "4.Entrar na fila do caixa\n" +
+                "5.Conferir o valor em dinheiro\n" +
+                "6.Entrar no banco\n" +
+                "7.Informar o valor a ser retirado\n" +
+                "8.Procurar a fila do caixa\n" +
+                "9.Pegar o comprovante, o cartão e o dinheiro\n" +
+                "10.Entregar o cartão ao funcionário que atende no caixa\n");
+        
+        return ...;
+    }
+
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     
